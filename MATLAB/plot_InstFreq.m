c5a36d7d72f73d435218ed3e094d8d2f19af9642
latudimas <riswandha.ld@gmail.com>
2018-01-19 00:04:02 +0700

    Instantaneous plot

---
+++ MATLAB/plot_InstFreq.m
@@ -0,0 +1,16 @@
+close all;
+
+load('matdata/SubjekData_Ahmad.mat');
+
+fs = 200;
+tPlot = -200:1000/200:800-1;
+x = SubjekData(1).IMF_Kongruen;
+figure;
+hold on;
+for i = 1: size(x, 1)/2;
+  temp = x(i,:);
+
+  z = hilbert(temp);
+  inst = fs/(2*pi)*diff(unwrap(angle(z)));
+  plot(tPlot(2:end), inst);
+end
