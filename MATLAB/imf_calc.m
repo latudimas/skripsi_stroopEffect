3a3e0932b72c0233e4c02bc7ea1a0ae888568fdb
latudimas <riswandha.ld@gmail.com>
2018-02-17 22:52:35 +0700

    Noe 17-2 10:52

--- MATLAB/imf_calc.m
+++ MATLAB/imf_calc.m
@@ -12,23 +12,52 @@ clear;
 % subjek = {'Ipon', 'Nia'};
 % fileNameNew = sprintf('matdata/GrandAverage_Female.mat'); % For Female Averaging Calculation
 
-% for sub = 1: length(subjek)
-  % fileNameOrigin = sprintf('matdata/SubjekData_%s.mat', subjek{sub});
-  fileNameOrigin = sprintf('matdata/SubjekData_Nia.mat')
-  load(fileNameOrigin);
+%%-----------------------------------------------------------------------
+% % for sub = 1: length(subjek)
+%   % fileNameOrigin = sprintf('matdata/SubjekData_%s.mat', subjek{sub});
+%   fileNameOrigin = sprintf('matdata/SubjekData_Nia.mat')
+%   load(fileNameOrigin);
 
-  for ch = 1:4
-    % Kongruen
-    temp = emd(SubjekData(ch).meanK_EMD);
-    SubjekData(ch).IMF_Kongruen = temp;
+%   for ch = 1:4
+%     % Kongruen
+%     temp = emd(SubjekData(ch).meanK_EMD);
+%     SubjekData(ch).IMF_Kongruen = temp;
 
-    % Inkongruen
-    temp = emd(SubjekData(ch).meanI_EMD);
-    SubjekData(ch).IMF_Inkongruen = temp;
+%     % Inkongruen
+%     temp = emd(SubjekData(ch).meanI_EMD);
+%     SubjekData(ch).IMF_Inkongruen = temp;
 
-    % Netral
-    temp = emd(SubjekData(ch).meanN_EMD);
-    SubjekData(ch).IMF_Netral = temp;
-  end
-  save(fileNameOrigin, 'SubjekData');
-% end
+%     % Netral
+%     temp = emd(SubjekData(ch).meanN_EMD);
+%     SubjekData(ch).IMF_Netral = temp;
+%   end
+%   save(fileNameOrigin, 'SubjekData');
+% % end
+
+%%-----------------------------------------------------------------------
+load('matdata/FusionData.mat');
+
+for ch = 1:4
+  %% KONGRUEN Condition
+  temp = FusionData(ch).K(1,:); % Male Subjek
+  FusionData(ch).imfKMale = emd(temp);
+
+  temp = FusionData(ch).K(2,:); % Female Subjek
+  FusionData(ch).imfKFemale = emd(temp);
+
+  %% INKONGRUEN
+  temp = FusionData(ch).I(1,:); % Male Subjek
+  FusionData(ch).imfIMale = emd(temp);
+
+  temp = FusionData(ch).I(2,:); % Female Subjek
+  FusionData(ch).imfIFemale = emd(temp);
+
+  %% NETRAL Condition
+  temp = FusionData(ch).N(1,:); % Male Subjek
+  FusionData(ch).imfNMale = emd(temp);
+
+  temp = FusionData(ch).N(2,:); % Female Subjek
+  FusionData(ch).imfNFemale = emd(temp);
+end
+
+save('matdata/FusionData.mat', 'FusionData');
--- MATLAB/plot_imf.m
+++ MATLAB/plot_imf.m
@@ -1,24 +1,54 @@
 %% Plotting IMF
 % Membuat plot berdasarkan IMF yang dihasilkan
 
-close all;
-clear;
+% close all;
+% clear;
 
-subjek = {'Ahmad', 'Jaler', 'Mudin', 'Ricahyo'};
-tPlot = -200:1000/200:800-1;
+% subjek = {'Ahmad', 'Jaler', 'Mudin', 'Ricahyo'};
+% tPlot = -200:1000/200:800-1;
 
-for sub = 1:length(subjek)
-  fileNameOrigin = sprintf('matdata/SubjekData_%s.mat', subjek{sub});
-  load(fileNameOrigin);
+% for sub = 1:length(subjek)
+%   fileNameOrigin = sprintf('matdata/SubjekData_%s.mat', subjek{sub});
+%   load(fileNameOrigin);
 
-  for ch = 1:4
-    h = figure('Name', sprintf('IMF Netral Ch %d %s', ch, subjek{sub}));
-    %Plot IMF dan FFT
-    temp = SubjekData(ch).IMF_Netral;
-    s = size(temp, 1);
-    for row = 1:s
-      subplot(s, 1, row); plot(tPlot, temp(row,:));
-    end
-    saveas(h, sprintf('Pictures/IMF/IMF Netral Ch %d %s', ch, subjek{sub}));
+%   for ch = 1:4
+%     h = figure('Name', sprintf('IMF Netral Ch %d %s', ch, subjek{sub}));
+%     %Plot IMF dan FFT
+%     temp = SubjekData(ch).IMF_Netral;
+%     s = size(temp, 1);
+%     for row = 1:s
+%       subplot(s, 1, row); plot(tPlot, temp(row,:));
+%     end
+%     saveas(h, sprintf('Pictures/IMF/IMF Netral Ch %d %s', ch, subjek{sub}));
+%   end
+% end
+
+%%-----------------------------------------------------------------------
+
+fileName = 'matdata/FusionData.mat';
+load(fileName);
+
+tPlot = -200:1000/200:799;
+
+for ch = 1:4
+  %% KONGRUEN
+  % Male Subjek
+  temp = FusionData(ch).imfKMale;
+  s = size(temp,1);
+  h = figure('Name', sprintf('IMF MALE Ch %d', ch));
+  % figure; title(sprintf('IMF MALE Ch %d, ch'));
+  for row = 1:s
+    subplot(s,1,row); plot(tPlot,temp(row,:));
   end
+  
+  % Female Subjek
+  temp = FusionData(ch).imfKFemale;
+  s = size(temp,1);
+  h = figure('Name', sprintf('IMF FEMALE Ch %d', ch));
+  % figure; title(sprintf('IMF FEMALE Ch %d, ch'));
+  for row = 1:s
+    subplot(s,1,row); plot(tPlot,temp(row,:));
+  end
+
+  
 end
