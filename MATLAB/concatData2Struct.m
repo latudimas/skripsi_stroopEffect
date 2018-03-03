53e3ea9a609a0f61706cb251a0e240377943e002
latudimas <riswandha.ld@gmail.com>
2018-03-03 18:43:43 +0700

    Female Grand Average Struct

---
+++ MATLAB/concatData2Struct.m
@@ -0,0 +1,51 @@
+% moving data from "SubjekData" Struct to "GrandAverage_M/F" Struct
+
+% Lokasi Data Target
+fileTarget = 'matdata/GrandAverage_Female.mat';
+if exist(fileTarget) == 2
+    disp(sprintf('File Exist: %s ', fileTarget));
+else
+    GrandAverage = struct('meanK_ERP', [], 'meanI_ERP', [], 'meanN_ERP', []);
+    
+    % Space Up
+    GrandAverage(1).meanK_ERP=[];
+    GrandAverage(2).meanK_ERP=[];
+    GrandAverage(3).meanK_ERP=[];
+    GrandAverage(4).meanK_ERP=[];
+
+    % Save struct sebagai matfile
+    save(fileTarget, 'GrandAverage');
+    disp('Struct Created');
+end
+
+% Nama Subjek FEMALE
+% subName = {'Nia', 'Ipon'}; % #1
+subName = {'Peni'}; % #2
+
+for sub = 1: length(subName);
+    % Buka struct data subjek
+    fileOrigin = sprintf('matdata/SubjekData_%s.mat', subName{sub});
+    disp(sprintf('Open %s Struct', subName{sub}));
+    load(fileOrigin);    
+
+    %Proses penyalinan data tiap kondisi dan tiap channel
+    for ch = 1:4
+        disp(sprintf('Channel : %d', ch ));
+        % KONGRUEN
+        temp = SubjekData(ch).meanK_ERP; % Peni
+        % temp = SubjekData(ch).NewMeanK_ERP; % Nia dan Ipon only
+        GrandAverage(ch).meanK_ERP =cat(1, GrandAverage(ch).meanK_ERP, temp); 
+
+        % INKONGRUEN
+        temp = SubjekData(ch).meanI_ERP; % Peni
+        % temp =  SubjekData(ch).NewMeanI_ERP; % Nia dan Ipon only
+        GrandAverage(ch).meanI_ERP =cat(1, GrandAverage(ch).meanI_ERP, temp); 
+
+        % NETRAL
+        temp = SubjekData(ch).meanN_ERP; % Peni
+        % temp =  SubjekData(ch).NewMeanN_ERP; % Nia dan Ipon only
+        GrandAverage(ch).meanN_ERP =cat(1, GrandAverage(ch).meanN_ERP, temp); 
+    end
+end
+
+save(fileTarget, 'GrandAverage');
--- MATLAB/plotGrandAverage.m
+++ MATLAB/plotGrandAverage.m
@@ -10,55 +10,63 @@ load('matdata/GrandAverage_Female.mat');
 
 tPlot = -200:1000/200:799;
 cPlot = ['b', 'g', 'r', 'm', 'k'];
+n = 0.1
 
 for ch=1:4
-  %% Kongruen Plot
-  figure;
-  for row = 1:size(GrandAverage_Female(ch).meanK_ERP, 1)
-    plot(tPlot, GrandAverage_Female(ch).meanK_ERP(row,:), 'Color', cPlot(row));
-    title(sprintf('Average Female Subject Kongruen Channel %d', ch));
-%     title(sprintf('Average Male Subject Kongruen Channel %d', ch));
-    xlabel('Waktu(ms)'); ylabel('Amplitudo')
-    hold on;
-  end
-  hold off;
-%   legend ('Ahmad', 'Jaler', 'Mudin', 'Ricahyo');
-  legend ('Nia', 'Ipon');
+%   %% Kongruen Plot
+%   figure;
+%   for row = 1:size(GrandAverage(ch).meanK_ERP, 1)
+%     plot(tPlot, GrandAverage(ch).meanK_ERP(row,:), 'Color', cPlot(row));
+%     title(sprintf('Average Female Subject Kongruen Channel %d', ch));
+% %     title(sprintf('Average Male Subject Kongruen Channel %d', ch));
+%     xlabel('Waktu(ms)'); ylabel('Amplitudo')
+%     hold on;
+%   end
+%   hold off;
+% %   legend ('Ahmad', 'Jaler', 'Mudin', 'Ricahyo');
+%   legend ('Nia', 'Ipon', 'Peni');
 
-  %% Inkongruen Plot
-  figure;
-  for row = 1:size(GrandAverage_Female(ch).meanI_ERP, 1)
-    plot(tPlot, GrandAverage_Female(ch).meanI_ERP(row,:), 'Color', cPlot(row));
-    title(sprintf('Average Female Subject Inkongruen Channel %d', ch));
-%     title(sprintf('Average Male Subject Inkongruen Channel %d', ch));
-    xlabel('Waktu(ms)'); ylabel('Amplitudo')
-    hold on;
-  end
-  hold off;
-%   legend ('Ahmad', 'Jaler', 'Mudin', 'Ricahyo');
-  legend ('Nia', 'Ipon');
+%   %% Inkongruen Plot
+%   figure;
+%   for row = 1:size(GrandAverage(ch).meanI_ERP, 1)
+%     plot(tPlot, GrandAverage(ch).meanI_ERP(row,:), 'Color', cPlot(row));
+%     title(sprintf('Average Female Subject Inkongruen Channel %d', ch));
+% %     title(sprintf('Average Male Subject Inkongruen Channel %d', ch));
+%     xlabel('Waktu(ms)'); ylabel('Amplitudo')
+%     hold on;
+%   end
+%   hold off;
+% %   legend ('Ahmad', 'Jaler', 'Mudin', 'Ricahyo');
+%   legend ('Nia', 'Ipon', 'Peni');
 
-  %% Netral Plot
-  figure;
-  for row = 1:size(GrandAverage_Female(ch).meanN_ERP, 1)
-    plot(tPlot, GrandAverage_Female(ch).meanN_ERP(row,:), 'Color', cPlot(row));
-    title(sprintf('Average Female Subject Netral Channel %d', ch));
-%     title(sprintf('Average Male Subject Netral Channel %d', ch));
-    xlabel('Waktu(ms)'); ylabel('Amplitudo')
-    hold on;
-  end
-  hold off;
-%   legend ('Ahmad', 'Jaler', 'Mudin', 'Ricahyo');
-  legend ('Nia', 'Ipon');
+%   %% Netral Plot
+%   figure;
+%   for row = 1:size(GrandAverage(ch).meanN_ERP, 1)
+%     plot(tPlot, GrandAverage(ch).meanN_ERP(row,:), 'Color', cPlot(row));
+%     title(sprintf('Average Female Subject Netral Channel %d', ch));
+% %     title(sprintf('Average Male Subject Netral Channel %d', ch));
+%     xlabel('Waktu(ms)'); ylabel('Amplitudo')
+%     hold on;
+%   end
+%   hold off;
+% %   legend ('Ahmad', 'Jaler', 'Mudin', 'Ricahyo');
+%   legend ('Nia', 'Ipon', 'Peni');
 
   %% Grand Average Tiap Kondisi
-  meanK = mean(GrandAverage_Female(ch).meanK_ERP);
-  meanI = mean(GrandAverage_Female(ch).meanI_ERP);
-  meanN = mean(GrandAverage_Female(ch).meanN_ERP);
+  GrandAvgK = mean(GrandAverage(ch).meanK_ERP);
+  GrandAvgI = mean(GrandAverage(ch).meanI_ERP);
+  GrandAvgN = mean(GrandAverage(ch).meanN_ERP);
   figure;
-  plot(tPlot, meanK, tPlot, meanI, tPlot, meanN);
+  plot(tPlot, GrandAvgK, tPlot, GrandAvgI, tPlot, GrandAvgN); % Non Smoothing Data
+  figure;
+  plot(tPlot, smooth(GrandAvgK, n, 'moving'), tPlot, smooth(GrandAvgI, n, 'moving'), tPlot, smooth(GrandAvgN, n, 'moving'));
   title(sprintf('Grand Average Female Subject All Condition Channel %d', ch));
   xlabel('Waktu(ms)'); ylabel('Amplitudo')
   legend('Kongruen', 'Inkongruen', 'Netral')
 
+  % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  % GrandAverage(ch).GrandAvg_Kongruen = GrandAvgK;
+  % GrandAverage(ch).GrandAvg_Inkongruen = GrandAvgI;
+  % GrandAverage(ch).GrandAvg_Netral = GrandAvgN;
+  % save('matdata/GrandAverage_Female.mat', 'GrandAverage');
 end
