1211c4c6f2085c153a13d5cd403554955e9a360b
latudimas <riswandha.ld@gmail.com>
2018-03-10 21:35:35 +0700

    save plotting Fusion Data

--- MATLAB/plotFusionData.m
+++ MATLAB/plotFusionData.m
@@ -1,5 +1,6 @@
 %% Plot Data FusionData
 % Data yang di plot Male vs Female
+close all; clear;
 
 load('matdata/FusionData.mat');
 tPlot = -200:1000/200:799;
@@ -7,30 +8,33 @@ for ch = 1:4
     % KONGRUEN
     temp = FusionData(ch).Kongruen_ERP;
     % temp = smooth(temp, 0.1, 'moving');
-    figure;
+    fig = figure;
     plot(tPlot, temp(1,:), tPlot, temp(2,:));
     % plot(tPlot, smooth(temp(1,:), 0.1, 'moving'), tPlot, smooth(temp(2,:), 0.1, 'moving')); %SMOOTHING DATA
     title(sprintf('Rerata Total Subjek Laki-Laki vs Perempuan KONGRUEN Channel %d', ch));
     xlabel('Waktu(ms)'); ylabel('Amplitudo (\muV)');
     legend('L', 'P');
+    saveas(fig, sprintf('Pictures/FusionData/Fusion Data Kongruen %d.png', ch));
 
     % INKONGRUEN
     temp = FusionData(ch).Inkongruen_ERP;
     % temp = smooth(temp, 0.1, 'moving');
-    figure;
+    fig = figure;
     plot(tPlot, temp(1,:), tPlot, temp(2,:));
     % plot(tPlot, smooth(temp(1,:), 0.1, 'moving'), tPlot, smooth(temp(2,:), 0.1, 'moving')); %SMOOTHING DATA
     title(sprintf('Rerata Total Subjek Laki-Laki vs Perempuan INKONGRUEN Channel %d', ch));
     xlabel('Waktu(ms)'); ylabel('Amplitudo (\muV)');
     legend('L', 'P');
+    saveas(fig, sprintf('Pictures/FusionData/Fusion Data Inkongruen %d.png', ch));
 
     % NETRAL
     temp = FusionData(ch).Netral_ERP;
     % temp = smooth(temp, 0.1, 'moving');
-    figure;
+    fig = figure;
     plot(tPlot, temp(1,:), tPlot, temp(2,:));
     % plot(tPlot, smooth(temp(1,:), 0.1, 'moving'), tPlot, smooth(temp(2,:), 0.1, 'moving')); %SMOOTHING DATA
     title(sprintf('Rerata Total Subjek Laki-Laki vs Perempuan NETRAL Channel %d', ch));
     xlabel('Waktu(ms)'); ylabel('Amplitudo (\muV)');
     legend('L', 'P');
+    saveas(fig, sprintf('Pictures/FusionData/Fusion Data Netral %d.png', ch));
 end
