ffb57df783971d66dd9c1138a6cce9f730e6d3b7
latudimas <riswandha.ld@gmail.com>
2018-03-20 11:42:39 +0700

    Edit erp plot line width and font size

--- MATLAB/EnergyCalculation.m
+++ MATLAB/EnergyCalculation.m
@@ -1,6 +1,6 @@
 %% CALCULATE SIGNAL ENERGY FROM EVERY SUBJECT ALL CHANNEL
 close all; clear all;
-N = 200; % Banyak data atau lebar data, lihat notebook lengkapnya
+N = 160; % Banyak data atau lebar data, lihat notebook lengkapnya
 
 %% SUBJECT
 subjek = {'Ahmad', 'Jaler', 'Mudin', 'Ricahyo', 'Nia', 'Peni', 'Sania', 'Nia2'};
@@ -42,34 +42,3 @@ end
 
 save(outputFile, 'Energy_ERP');
 
-% %% FEMALE SUBJECT
-% disp('Process Female Subject Now')
-% subjekFemale ={'Nia', 'Ipon'};
-
-% for sub = 1:length(subjekFemale)
-%     % LOAD DATA
-%     fileName = sprintf('matdata/SubjekData_%s.mat', subjekFemale{sub})
-%     load(fileName);
-
-%     % CHANNEL ITERATION
-%     for ch = 1:4
-%         % KONGRUEN
-%         temp = SubjekData(ch).meanK_ERP;
-%         rmsK = rms(temp); % calc RMS signal
-%         energyK = rmsK*rmsK*N; % hitung energy
-%         SubjekData(ch).energyK = energyK; % tambah data ke struct 
-
-%         % INKONGRUEN
-%         temp = SubjekData(ch).meanI_ERP;
-%         rmsI = rms(temp); % calc RMS signal
-%         energyI = rmsI*rmsI*N;% hitung energy
-%         SubjekData(ch).energyI = energyI; % tambah data ke struct
-
-%         % NETRAL
-%         temp = SubjekData(ch).meanN_ERP;
-%         rmsN = rms(temp); % calc RMS signal
-%         energyN = rmsN*rmsN*N; % hitung energy
-%         SubjekData(ch).energyN = energyN; % tambah data ke struct
-%     end
-%     save(fileName, 'SubjekData');
-% end
